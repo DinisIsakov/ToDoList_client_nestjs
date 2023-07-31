cab3eedbf6f31e448ab8dc823fbdac3931129dc5
Dinis Isakov <isakovdr@gmail.com>
2023-07-31 09:46:52 +0300

    <create client todolist>

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { rootReducer } from './redux/rootReducer';
+import { sagaWatcher } from './redux/saga/sagas';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const saga = createSagaMiddleware();
+
+const store = createStore(rootReducer, compose(applyMiddleware(saga)));
+
+saga.run(sagaWatcher);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
